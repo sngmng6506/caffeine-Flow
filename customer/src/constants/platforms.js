14de56d11bb049ffaa703ebc800e1539c5ef8a6b
sngmng6506 <110221311+sngmng6506@users.noreply.github.com>
2026-07-09 14:48:34 +0900

    refactor: centralize customer platform constants

---
+++ customer/src/constants/platforms.js
@@ -0,0 +1,23 @@
+export const PLATFORM = Object.freeze({
+  YOUTUBE: 'youtube',
+  SOUNDCLOUD: 'soundcloud',
+  SPOTIFY: 'spotify',
+});
+
+export const VALID_PLATFORMS = Object.freeze(Object.values(PLATFORM));
+
+export const PLATFORM_BADGE = Object.freeze({
+  [PLATFORM.YOUTUBE]: { label: 'YouTube', bg: '#ff0000', color: '#fff' },
+  [PLATFORM.SOUNDCLOUD]: { label: 'SoundCloud', bg: '#ff5500', color: '#fff' },
+  [PLATFORM.SPOTIFY]: { label: 'Spotify', bg: '#1db954', color: '#fff' },
+});
+
+export const PLATFORM_LINKS = Object.freeze([
+  { id: PLATFORM.YOUTUBE, href: 'https://www.youtube.com' },
+  { id: PLATFORM.SPOTIFY, href: 'https://open.spotify.com' },
+  { id: PLATFORM.SOUNDCLOUD, href: 'https://soundcloud.com' },
+]);
+
+export function platformLabel(platform) {
+  return PLATFORM_BADGE[platform]?.label || platform;
+}
